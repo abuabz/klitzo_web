d2daa0b78ef6a71eb62c80f274770586719b4560
ABU <mohamedaboobackerp@gmail.com>
2026-02-25 16:01:53 +0530

    updated 2 products

--- app/page.tsx
+++ app/page.tsx
@@ -99,7 +99,7 @@ export default function KlitzoLanding() {
     },
     {
       id: 5,
-      name: "KLITZO Helmet Deodorizer 100ml",
+      name: "KLITZO Helmet Freshener 100ml",
       image: "/assets/helmet01.jpeg",
       price: "₹399.00",
       originalPrice: "₹699.00",
