2213196cbbaa2943694a455f69d08969d89c9281
abu <mohamedaboobackerp@gmail.com>
2026-09-14 18:38:15 +0530

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,6 +66,7 @@ export const viewport: Viewport = {
 };
 
 import { GoogleProvider } from '@/components/google-provider'
+import FacebookPixel from '@/components/facebook-pixel'
 
 export default function RootLayout({
   children,
@@ -85,29 +86,7 @@ html {
       </head>
       <body>
         <GoogleProvider>
-          <Script id="meta-pixel" strategy="afterInteractive">
-            {`
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1557548879710692');
-              fbq('track', 'PageView');
-            `}
-          </Script>
-          <noscript>
-            <img
-              height="1"
-              width="1"
-              style={{ display: "none" }}
-              src="https://www.facebook.com/tr?id=1557548879710692&ev=PageView&noscript=1"
-              alt=""
-            />
-          </noscript>
+          <FacebookPixel />
           <AdminHide>
             <Header />
           </AdminHide>
